904ef5d267f576ed67e524a66caf5cb838e3a4af
Rie Saito <2201140071bw@cyber-u.ac.jp>
2026-01-04 08:11:31 +0900

    add hello page

--- backup_app.js
+++ backup_app.js
@@ -6,10 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var HelloRouter = require('./routes/hello');
 
 var app = express();
-var PORT = 30044;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/hello', helloRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -47,7 +44,7 @@ module.exports = app;
 const express = require('express');
 const app = express();
 
-var PORT = 30044;
+const PORT = 30044;
 
 app.get('/', (req, res) => {
   res.send(`
