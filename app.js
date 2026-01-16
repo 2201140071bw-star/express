ad09148b62dcfd751b47059aa4c19d77547a363d
Rie Saito <2201140071bw@cyber-u.ac.jp>
2026-01-17 05:43:22 +0900

    add mongodb access

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var HelloRouter = require('./routes/hello');
+var helloRouter = require('./routes/hello');
+var notesRouter = require('./routes/notes');
 
 var app = express();
 var PORT = 30044;
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
+app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
